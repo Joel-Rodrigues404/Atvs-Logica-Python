3f8fc47bd824fcd35401b7a2f260cd1f0e4817e1
Joel-Rodrigues <joelsandersonrodrigues@gmail.com>
2023-10-17 11:46:21 -0300

    UPp arquivos

---
+++ 3_Estrutura_de_repeticao/atv34.py
@@ -0,0 +1,17 @@
+"""
+Os números primos possuem várias aplicações dentro da Computação, por exemplo na Criptografia. 
+Um número primo é aquele que é divisível apenas por um e por ele mesmo. Faça um programa que peça um número inteiro 
+e determine se ele é ou não um número primo.
+"""
+
+num = abs(int(input("digite um numero: ")))
+
+cont = 1
+divisores = 0
+while cont <= num:
+    if num % cont == 0:
+        divisores += 1
+    cont += 1
+
+if divisores == 2:
+    print(f'O numero {num} e primo!')
---
+++ 3_Estrutura_de_repeticao/atv35.py
@@ -0,0 +1,30 @@
+"""
+Encontrar números primos é uma tarefa difícil. Faça um programa que gera uma lista dos números primos existentes 
+entre 1 e um número inteiro informado pelo usuário
+"""
+
+num = abs(int(input('digite um numero: ')))
+
+# cont = 1
+# divisores = 0
+# lista_primos = list()
+# while cont <= num:
+#     if num % cont == 0:
+#         divisores += 1
+#         lista_primos.append(cont)
+#     cont += 1
+
+# if divisores == 2:
+#     print("primo")
+#     print(lista_primos)
+
+primos = list()
+divisores = [1,2,3,5,7,11]
+div = 0
+for x in range(1, num + 1):
+    for y in divisores:
+        if x % y == 0:
+            div += 1
+
+print(primos)
+
