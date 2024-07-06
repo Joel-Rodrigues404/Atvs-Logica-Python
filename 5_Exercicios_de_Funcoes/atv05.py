1896c66e2ab2af9ceede6293a5528054159071dd
Joel Rodrigues <joelsandersonrodrigues@gmail.com>
2024-07-05 23:01:35 -0300

    atividades funçoes

--- 5_Exercicios_de_Funcoes/atv05.py
+++ 5_Exercicios_de_Funcoes/atv05.py
@@ -2,4 +2,7 @@
 Faça um programa com uma função chamada somaImposto. A função possui dois parâmetros formais: taxaImposto, que é a quantia de imposto sobre vendas expressa em porcentagem e custo, que é o custo de um item antes do imposto. A função “altera” o valor de custo para incluir o imposto sobre vendas.
 """
 
-def soma_imposto()
+
+def soma_imposto(taxa_imposto, custo):
+    novo_valor = taxa_imposto * custo
+    return novo_valor
