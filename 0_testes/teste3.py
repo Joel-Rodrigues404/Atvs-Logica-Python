a251e0d3e775244657ceb923ee129235f33c168d
Joel-Rodrigues404 <joelsandersonrodrigues@gmail.com>
2024-11-26 11:19:35 -0300

    upando arquivos testes

--- 0_testes/teste3.py
+++ 0_testes/teste3.py
@@ -1,3 +1,16 @@
-variaveis = [(1, 2, 3), (4, 5, 6)]
+# variaveis = [(1, 2, 3), (4, 5, 6)]
+#
+# print(','.join(*variaveis))
 
-print(','.join(*variaveis))
+#
+# def prox_rest(valor):
+#     print(valor // 2)
+#     print(valor % 2)
+#
+#
+# prox_rest(176)
+
+import ast
+
+string = 'teste'
+print(ast.dump(ast.parse(f'x = 1; y = 2, {string}', mode='single'), indent=4))
